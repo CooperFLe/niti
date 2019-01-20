b16d011cb9c79975f79df9c9fa1590bd2de7056d
Cooper F Le <cooperfle@utdallas.edu>
2019-01-19 20:01:52 -0600

    Updates

---
+++ precache-manifest.0700a1ed6e442edd100eec12feb93639.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "757b31114d1b479c36f2",
+    "url": "/niti/static/js/runtime~main.757b3111.js"
+  },
+  {
+    "revision": "d33c0696556e6f490841",
+    "url": "/niti/static/js/main.d33c0696.chunk.js"
+  },
+  {
+    "revision": "693a62df80ea09a742b2",
+    "url": "/niti/static/js/1.693a62df.chunk.js"
+  },
+  {
+    "revision": "d33c0696556e6f490841",
+    "url": "/niti/static/css/main.9cdf794b.chunk.css"
+  },
+  {
+    "revision": "121f78961c290d4bdbabc89238eac31c",
+    "url": "/niti/index.html"
+  }
+];
